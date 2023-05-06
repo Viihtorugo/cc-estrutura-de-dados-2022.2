9e10a7f1a4b3f7255e5b099294024acfc85d20b3
Viihtorugo <Victor-Torugo@hotmail.com>
2023-05-05 21:15:28 -0300

    Refatorando selection sort v2

--- prova/ab2/exe02.c
+++ prova/ab2/exe02.c
@@ -13,6 +13,88 @@ typedef struct graph {
     list_adj **list_adj;
 } graph;
 
+typedef struct queue {
+    int v;
+    struct queue *next;
+} queue;
+
+void enqueue (int v, queue **q)
+{
+    queue *new_node = (queue *) malloc(sizeof(queue));
+    
+    if (new_node == NULL)
+        exit(1);
+
+    new_node->v = v;
+    new_node->next = NULL;
+
+    if (*q == NULL)
+    {
+        *q = new_node;
+    }
+    else
+    {
+        queue *aux = *q;
+
+        while (aux->next != NULL)
+            aux = aux->next;
+
+        aux->next = new_node;
+    }
+}
+
+void enqueue_priority (int v, queue **q)
+{
+    queue *new_node = (queue *) malloc(sizeof(queue));
+    
+    if (new_node == NULL)
+        exit(1);
+
+    new_node->v = v;
+    new_node->next = NULL;
+
+    if (*q == NULL)
+    {
+        *q = new_node;
+    }
+    else
+    {
+        queue *aux = *q;
+        queue *previous = NULL;
+
+        while (aux != NULL && v >= aux->v)
+        {
+            previous = aux;
+            aux = aux->next;
+        }
+
+        if (previous == NULL)
+        {
+            new_node->next = *q;
+            *q = new_node;
+        }
+        else
+        {
+            previous->next = new_node;
+            new_node->next = aux;
+        }
+    }
+}
+
+int dequeue (queue **q)
+{
+    queue *aux = *q;
+
+    int dequeued = aux->v;
+
+    (*q) = (*q)->next;
+
+    aux->next = NULL;
+    free(aux);
+
+    return dequeued;
+}
+
 list_adj *add_list_adj (int v, list_adj *head)
 {
     list_adj *new_node = (list_adj *) malloc(sizeof(list_adj));
@@ -30,11 +112,25 @@ list_adj *add_list_adj (int v, list_adj *head)
     else
     {
         list_adj *aux = head;
+        list_adj *previous = NULL;
 
-        while (aux->next != NULL)
+        while (aux != NULL && v >= aux->v)
+        {
+            previous = aux;
             aux = aux->next;
+        }
 
-        aux->next = new_node;
+        if (previous == NULL)
+        {
+            new_node->next = aux;
+            head = new_node;
+        }
+        else
+        {
+            previous->next = new_node;
+            new_node->next = aux;
+        }
+
         return head;
     }
 
@@ -63,7 +159,6 @@ graph *create_graph (int size)
 void add_vertex (int x, int y, graph *g)
 {
     g->list_adj[x] = add_list_adj(y, g->list_adj[x]);
-    g->list_adj[y] = add_list_adj(x, g->list_adj[y]);
 }
 
 void dfs (graph *g, int v, int count)
@@ -81,27 +176,207 @@ void dfs (graph *g, int v, int count)
             dfs(g, aux->v, count);
         
         aux = aux->next;
+    }   
+}
+
+void print_queue (queue *q)
+{
+    queue *aux = q;
+
+    while (aux != NULL)
+    {
+        if (aux->next == NULL)
+        {
+            printf("%d\n", aux->v);
+        }
+        else
+        {
+            printf("%d ", aux->v);
+        }
+
+        aux = aux->next;
     }
+}
+
+void print_graph(graph *g)
+{
+    for (int i = 0; i < g->size; i++)
+    {
+        printf("v%d -> ", i);
+
+        list_adj *aux = g->list_adj[i];
+
+        while (aux != NULL)
+        {
+            if (aux->next == NULL)
+            {
+                printf("v%d vizinhos", aux->v);
+            }
+            else
+            {
+                printf("v%d ", aux->v);
+            }
+
+            aux = aux->next;
+        }
+
+        printf("\n");
+        
+    }
+    
+}
+
+int search (list_adj *l, int elem)
+{
+    list_adj *aux = l;
+
+    while (aux != NULL && elem != aux->v)
+        aux = aux->next;
+    
+    return (int) (aux == NULL);
+}
+
+int bfs_count (graph *g, int v, int d)
+{
+    int count = 1;
+
+    if (v == d)
+        return 0;
+
+    queue *q = NULL;
+    enqueue(v, &q);
+
+    while (q != NULL)
+    {
+        int dequeued = dequeue(&q);
+        list_adj *aux = g->list_adj[dequeued];
+
+        if (search(aux, d) == 0)
+            return count;
+
+
+        while (aux != NULL)
+        {
+            if (g->visited[aux->v] == 0)
+            {
+                g->visited[aux->v] = 1;
+                enqueue(aux->v, &q);
+            }
+
+            aux = aux->next;
+        }
+
+        count += 1;
+
+        //printf("\nFila: ");
+        //print_queue(q);
+        //printf("\n");
+
+    }
+
+    return -1;
+}
+
+void bfs (graph *g, int v, int d)
+{
+    queue *q = NULL;
+    enqueue(v, &q);
+    g->visited[v] = 1;
+
+    while (q != NULL)
+    {
+        int dequeued = dequeue(&q);
+        list_adj *aux = g->list_adj[dequeued];
+        
+        printf("Iniciando busca em largura a partir de %d\n", dequeued);
+        
+        while (aux != NULL)
+        {
+
+            if (g->visited[aux->v] == 0)
+            {
+                g->visited[aux->v] = 1;
+                enqueue(aux->v, &q);
+            }
+            
+            aux = aux->next;
+        }
+        
+        print_queue(q);
+    }
+
+    printf("\n");
+    for (int i = 0; i < g->size; i++)
+    {
+        printf("%d | ", i);
+
+        for (int i = 0; i < g->size; i++)
+            g->visited[i] = 0;
+
+        int count = bfs_count(g, v, i);
+
+        if (count == -1)
+        {
+            printf("- | ");
+        }
+        else
+        {
+            printf("%d | ", count);
+        }
+
+        if (count == -1)
+        {
+            printf("-\n");
+        }
+        else
+        {
+            printf("%d\n", count);
+        }
+    }
+
+    printf("\nCaminho entre %d e %d:", v, d);
+
+    printf("\n");
     
 }
 
 int main ()
 {
 
-    int n, m;
-    scanf("%d %d", &n, &m);
-
-    graph *g = create_graph(n);
-
-    for (int i = 0; i < m; i++)
+    int n, m, e;
+    scanf("%d %d %d", &n, &m, &e);
+    
+    printf("--------\n\n");
+    if (n != 0)
     {
-        int x, y;
-        scanf("%d %d", &x, &y);
-        add_vertex(x, y, g);
-    }
+        graph *g = create_graph(n);
 
-    dfs(g, 0, 0);
-    printf("\n");
+        for (int i = 0; i < m; i++)
+        {
+            int x, y;
+            scanf("%d %d", &x, &y);
+            add_vertex(x, y, g);
+        }
+        for (int i = 0; i < e; i++)
+        {
+            int v, d;
+            scanf("%d %d", &v, &d);
+
+            printf("\n");
+            print_graph(g);
+            printf("\n");
+
+            for (int i = 0; i < g->size; i++)
+                g->visited[i] = 0;
+
+            printf("Caso de Teste #%d - BFS(%d)\n\n", i + 1, v);
+            //print_graph(0, g);
+            //dfs(g, 0, 0);
+            bfs(g, v, d);
+            printf("--------\n\n");
+        }
+
+    }
 
     return 0;
 }
