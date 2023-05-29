c7bea27ddc00ca9efd0c7db70b3885dd3439040f
Viihtorugo <Victor-Torugo@hotmail.com>
2023-05-29 05:38:02 -0300

    Adicionando a ultima questão com erro

--- projeto/parte-2/list/list_v1.c
+++ projeto/parte-2/list/list_v1.c
@@ -82,7 +82,7 @@ void print_elevadores(elevador *elevadores)
             }
             else if (elevadores[i].andar_atual < 100)
             {
-                 printf("Elevador 0%d: 0%d andar e %d corredor\n", i + 1, elevadores[i].andar_atual, elevadores[i].corredor_atual);
+                printf("Elevador 0%d: 0%d andar e %d corredor\n", i + 1, elevadores[i].andar_atual, elevadores[i].corredor_atual);
             }
             else
             {
@@ -97,25 +97,94 @@ void print_elevadores(elevador *elevadores)
             }
             else if (elevadores[i].andar_atual < 100)
             {
-                 printf("Elevador %d: 0%d andar e %d corredor\n", i + 1, elevadores[i].andar_atual, elevadores[i].corredor_atual);
+                printf("Elevador %d: 0%d andar e %d corredor\n", i + 1, elevadores[i].andar_atual, elevadores[i].corredor_atual);
             }
             else
             {
                 printf("Elevador %d: %d andar e %d corredor\n", i + 1, elevadores[i].andar_atual, elevadores[i].corredor_atual);
             }
         }
-
     }
 }
 
-int colisao (int andar, int corredor, elevador elevadores[])
+void tratamento_colisao(int i, int k, elevador elevadores[])
 {
-    int array[15] = {0}, aux = 0;
+    printf("\n---- TRATAMENTO DE COLISÃO ----\n\n");
 
-    for (int i = 0; i < 15; i++)
-        if (andar == elevadores[i].andar_atual && corredor == elevadores[i].corredor_atual)
-            return i;
+    while (elevadores[i].andar_atual % 10 != 0)
+    {
+        int prox_andar;
+
+        if (elevadores[i].subir)
+        {
+            prox_andar = elevadores[i].andar_atual + 1;
+        }
+        else
+        {
+            prox_andar = elevadores[i].andar_atual - 1;
+        }
+
+        printf("\n");
+        print_elevadores(elevadores);
+        printf("\n");
+
+        elevadores[i].andar_atual = prox_andar;
+
+        if (elevadores[i].ocupado == 0)
+            elevadores[i].andar_dest = prox_andar;
+    }
+
+    printf("\n\n----- MODIFICANDO O CORREDOR! -----\n");
     
+    int prox_corredor, atualizar_corretor;
+
+    if (elevadores[i].corredor_atual == 3)
+    {
+        atualizar_corretor = elevadores[i].corredor_atual;
+        elevadores[i].corredor_atual -= 1;
+    }
+    else
+    {
+        atualizar_corretor = elevadores[i].corredor_atual;
+        elevadores[i].corredor_atual += 1;
+    }
+
+
+    printf("\n");
+    print_elevadores(elevadores);
+    printf("\n");
+
+    printf("\n\n----- FIM DO TRATAMENTO DE COLISÃO -----\n");
+}
+
+int colisao(int index, elevador elevadores[])
+{
+
+    int count = 1;
+
+    while (count <= 10)
+    {
+        for (int i = 0; i < 15; i++)
+        {
+            int aux;
+
+            if (elevadores[index].subir)
+            {
+                aux = elevadores[index].andar_atual + count;
+            }
+            else
+            {
+                aux = elevadores[index].andar_atual - count;
+            }
+
+            if (aux == elevadores[i].andar_atual)
+                if (elevadores[i].corredor_atual == elevadores[index].corredor_atual)
+                    return i;
+        }
+
+        count += 1;
+    }
+
     return -1;
 }
 
@@ -130,76 +199,74 @@ int andar_elevador_dif_andar_dest(elevador elevadores[])
 
 void iniciar_elevadores(elevador elevadores[])
 {
-    while (andar_elevador_dif_andar_dest(elevadores))
+    for (int i = 0; i < 15; i++)
     {
-        for (int i = 0; i < 15; i++)
+        if ((elevadores[i].andar_atual != elevadores[i].andar_dest) || elevadores[i].ocupado)
         {
-            if ((elevadores[i].andar_atual != elevadores[i].andar_dest) || elevadores[i].ocupado)
+            if (elevadores[i].andar_atual == elevadores[i].andar_dest && elevadores[i].passageiros == 0)
             {
-                if (elevadores[i].andar_atual == elevadores[i].andar_dest && elevadores[i].passageiros == 0)
+                printf("\n---- ELEVADOR %d: ANDAR %d ----\n", i + 1, elevadores[i].andar_atual);
+                int andar_dest;
+                
+                while (1)
                 {
-                    printf("\n---- ELEVADOR %d: ANDAR %d ----\n", i + 1, elevadores[i].andar_atual);
-                    int andar_dest = get_number("\nDigite o andar que deseja ir: ");
+                    andar_dest = get_number("\nDigite o andar que deseja ir: ");
 
-                    elevadores[i].passageiros = 1;
-                    elevadores[i].andar_dest = andar_dest;
-
-                    if (elevadores[i].andar_atual > andar_dest)
+                    if (andar_dest > 300 || andar_dest < 1)
                     {
-                        elevadores[i].subir = 0;
+                        printf("\n---- ERRO ----\n");
+                        printf("\n1 - O sistema não suporta números negativos!\n");
+                        printf("2 - O sistema não suporta números acima de 300 ou abaixo 1, pois o intervalo dos andares é de 1 até 300!\n");
                     }
                     else
                     {
-                        elevadores[i].subir = 1;
+                        break;
                     }
                 }
-                else if (elevadores[i].andar_atual == elevadores[i].andar_dest && elevadores[i].passageiros)
+
+                elevadores[i].passageiros = 1;
+                elevadores[i].andar_dest = andar_dest;
+
+                if (elevadores[i].andar_atual > andar_dest)
                 {
-                    printf("\n----- ELEVADOR %d LIBERADO -----\n", i + 1);
-                    elevadores[i].passageiros = 0;
-                    elevadores[i].ocupado = 0;
+                    elevadores[i].subir = 0;
                 }
                 else
                 {
-                    int prox_andar = elevadores[i].andar_atual;
+                    elevadores[i].subir = 1;
+                }
+            }
+            else if (elevadores[i].andar_atual == elevadores[i].andar_dest && elevadores[i].passageiros)
+            {
+                printf("\n----- ELEVADOR %d LIBERADO -----\n", i + 1);
+                elevadores[i].passageiros = 0;
+                elevadores[i].ocupado = 0;
+            }
+            else
+            {
+                if (elevadores[i].subir)
+                {
+                    elevadores[i].andar_atual += 1;
+                }
+                else
+                {
+                    elevadores[i].andar_atual -= 1;
+                }
 
-                    if (elevadores[i].subir)
-                    {
-                        prox_andar += 1;
-                    }
-                    else
-                    {
-                        prox_andar -= 1;
-                    }
-                    
-                    int index = colisao(prox_andar, elevadores[i].corredor_atual, elevadores);
+                int index_colisao = colisao(i, elevadores);
 
-                    if (index != -1)
-                    {
-                        printf("\n---- COLISÃO ---- \n");
-
-
-                        printf("\n\nElevador %d: %d andar e %d corredor\n", i + 1, elevadores[i].andar_atual, elevadores[i].corredor_atual);
-                        printf("Elevador %d: %d andar e %d corredor\n\n", index + 1, elevadores[index].andar_atual, elevadores[index].corredor_atual);
-
-                        exit(1);
-                    }
-                    else
-                    {
-                        elevadores[i].andar_atual = prox_andar;
-                    }
+                if (index_colisao != -1)
+                {
+                    tratamento_colisao(i, index_colisao, elevadores);
                 }
             }
         }
-
-        printf("\n ---- Movimentos ---- \n\n");
-        print_elevadores(elevadores);
-        printf("\n");
-
     }
 
-
-    printf("\n---- FIM DAS REQUIÇÕES ----\n");
+    printf("\n ---- Movimentos ---- \n\n");
+    print_elevadores(elevadores);
+    printf("\n");
+    
 }
 
 int elevador_mais_prox(int andar_dest, elevador elevadores[])
@@ -224,18 +291,17 @@ int elevador_mais_prox(int andar_dest, elevador elevadores[])
         }
     }
 
-    
     printf("\nElevador %d: %d andar e %d corredor\n\n", menor + 1, elevadores[menor].andar_atual, elevadores[menor].corredor_atual);
     return menor;
 }
 
-int andar_de_destino_ja_foi_alocado (int andar_dest, elevador elevadores[])
+int andar_de_destino_ja_foi_alocado(int andar_dest, elevador elevadores[])
 {
 
     for (int i = 0; i < 15; i++)
-        if (elevadores[i].andar_dest == andar_dest)
+        if (elevadores[i].andar_dest == andar_dest && elevadores[i].ocupado)
             return i;
-    
+
     return -1;
 }
 
@@ -261,53 +327,40 @@ int main()
         elevadores[i].subir = 0;
     }
 
-    for (int i = 0; i < 15; i++)
+    int andar = 60;
+
+    for (int i = 0; i < 15; i += 3)
     {
-        int andar = 1, corredor = 1;
-
-        while (colisao(andar, corredor, elevadores) != -1)
-        {
-            andar = (rand() % 300) + 1;
-            corredor = (rand() % 3) + 1;
-            
-            if (andar % 10 == 0)
-            {
-                if (andar == 300)
-                {
-                    andar -= 1;
-
-                }
-                else
-                {
-                    andar += 1;
-                }
-            }
-        }
-
-
-        elevadores[i].andar_atual = andar;
-        elevadores[i].andar_dest = andar;
-        elevadores[i].corredor_atual = corredor;
+        elevadores[i].andar_atual = andar - 1;
+        elevadores[i].andar_dest = andar - 1;
+        elevadores[i].corredor_atual = 1;
+        elevadores[i + 1].andar_atual = andar - 2;
+        elevadores[i + 1].andar_dest = andar - 2;
+        elevadores[i + 1].corredor_atual = 2;
+        elevadores[i + 2].andar_atual = andar - 3;
+        elevadores[i + 2].andar_dest = andar - 3;
+        elevadores[i + 2].corredor_atual = 3;
+        andar += 60;
     }
 
     printf("\n---- POSIÇÃO INCIAL DOS ELEVADORES ----\n\n");
     print_elevadores(elevadores);
     printf("\n---------------------------------------\n");
-    
+
     while (1)
     {
-        int n;
+        int andar = 0;
 
         while (1)
         {
-            printf("\nInforme 0 para finalizar o sistema!\n");
-            n = get_number("Informe a quantidade de requições nos elevadores: ");
+            printf("Digite 0 caso não tenha requisição: ");
+            andar = get_number("\nDigite o andar da requisição: ");
 
-            if (n > 15 || n < 0)
+            if (andar > 300 || andar < 0)
             {
                 printf("\n---- ERRO ----\n");
                 printf("\n1 - O sistema não suporta números negativos!\n");
-                printf("2 - O sistema não suporta números acima de 15, pois só existe 15 elevadores!\n");
+                printf("2 - O sistema não suporta números acima de 300, pois o intervalo dos andares é de 1 até 300!\n");
             }
             else
             {
@@ -315,30 +368,8 @@ int main()
             }
         }
 
-
-        if (n == 0)
-            break;
-
-        for (int i = 0; i < n; i++)
+        if (andar != 0)
         {
-            int andar = 0; 
-            
-            while (1)
-            {
-                andar = get_number("\nDigite o andar que você se encontra: ");
-
-                if (andar > 300 || andar < 1)
-                {
-                    printf("\n---- ERRO ----\n");
-                    printf("\n1 - O sistema não suporta números negativos!\n");
-                    printf("2 - O sistema não suporta números acima de 300 ou abaixo 1, pois o intervalo dos andares é de 1 até 300!\n");
-                }
-                else
-                {
-                    break;
-                }
-            }
-
             int index = andar_de_destino_ja_foi_alocado(andar, elevadores);
 
             if (index == -1)
@@ -361,8 +392,8 @@ int main()
                 }
             }
             else
-            { 
-                printf("\nO elevador %d estará nesse andar!\n", index + 1);
+            {
+                printf("\nO elevador %d está nesse andar ou passará está ocupado!\n", index + 1);
 
                 elevadores[index].ocupado = 1;
                 elevadores[index].passageiros = 0;
@@ -370,10 +401,9 @@ int main()
         }
 
         iniciar_elevadores(elevadores);
-        
     }
-    
+
     printf("\n--------- FIM DO SISTEMA ---------\n");
-    
+
     return 0;
 }
